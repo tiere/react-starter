2fb2ebcb5a7b9066997c5e58ce50bea5ad8a0a40
Tommi Järvinen <tommi.tiere@gmail.com>
2017-02-06 19:52:13 +0000

    Use babel-loader for .js files

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,9 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
+  module: {
+    rules: [
+      {test: /\.js$/, use: "babel-loader"},
+    ],
+  },
 };
