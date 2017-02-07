7c65fa8391cdf1f2fb1cb1a3a98f2d9cf56d4b8d
Tommi Järvinen <tommi.tiere@gmail.com>
2017-02-07 20:50:25 +0000

    Setup redux-devtools browser extension

--- src/containers/root.js
+++ src/containers/root.js
@@ -5,7 +5,10 @@ import React from "react";
 import {createStore} from "redux";
 import reducers from "../reducers";
 
-const store = createStore(reducers);
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const Root = (props) =>
   <Provider store={store}>
