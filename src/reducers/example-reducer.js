4f2b2cf5b9f4664071224d1a2e0124aff41fbcbe
Tommi Järvinen <tommi.tiere@gmail.com>
2017-02-07 20:41:37 +0000

    Handle example action in example reducer

--- src/reducers/example-reducer.js
+++ src/reducers/example-reducer.js
@@ -1,8 +1,16 @@
+import {EXAMPLE_ACTION} from "../actions/types";
+
 const initialState = {
   example: "",
 };
 
-const exampleReducer = (state = initialState, action) =>
-  state;
+const exampleReducer = (state = initialState, action) => {
+  switch (action.type) {
+  case EXAMPLE_ACTION:
+    return Object.assign({}, state, {example: action.text});
+  default:
+    return state;
+  }
+};
 
 export default exampleReducer;
