3cc8f7edd3221353211098768e51e030046a2866
Tommi Järvinen <tommi.tiere@gmail.com>
2017-02-11 16:59:43 +0000

    Test exampleReducer when action type is EXAMPLE_ACTION

--- test/reducers/example-reducer.test.js
+++ test/reducers/example-reducer.test.js
@@ -1,7 +1,13 @@
+import * as actionTypes from "../../src/actions/types";
 import exampleReducer from "../../src/reducers/example-reducer";
 
 describe("exampleReducer", () => {
   test("initial state", () => {
     expect(exampleReducer(undefined, {})).toEqual({example: ""});
+  });
+
+  test ("when action type is EXAMPLE_ACTION", () => {
+    expect(exampleReducer(undefined, {type: actionTypes.EXAMPLE_ACTION, text: "Testing"}))
+      .toEqual({example: "Testing"});
   });
 });
